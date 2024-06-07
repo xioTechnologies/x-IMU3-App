243535e570354a661e8e8869afa93c66ffaef5f6
Jelle Bakker <jellebakker@jb-audio.com>
2024-06-07 15:33:33 +0200

    Handle the response for multiple commands

--- lib/features/commands/data/datasource/commands_datasource.dart
+++ lib/features/commands/data/datasource/commands_datasource.dart
@@ -23,35 +23,33 @@ class CommandsAPI extends API {
   static Future<List<CommandMessage>> sendCommandAsync(List<CommandMessage> commands, Pointer<XIMU3_Connection> connection) async {
     List<CommandMessage> responses = [];
 
-    for (final CommandMessage command in commands) {
-      final response = Completer<List<String>>();
+    final completer = Completer<XIMU3_CharArrays>();
 
-      void callback(XIMU3_CharArrays data, ffi.Pointer<ffi.Void> context) {
-        response.complete(_toListOfStringsAndFree(data));
-      }
-
-      ffi.Pointer<ffi.Pointer<Utf8>> pointer = FFIHelpers.convertListToPointerPointer([command.toJson()]);
-      _instance.api.XIMU3_connection_send_commands_async(connection, pointer.cast(), 1, 2, 500, NativeCallable<
-          ffi.Void Function(XIMU3_CharArrays data,
-              ffi.Pointer<ffi.Void> context)>.listener(callback)
-          .nativeFunction,
-          ffi.nullptr);
-      calloc.free(pointer);
-
-      await response.future.then((response) {
-          if (response.isNotEmpty) {
-              responses.add(CommandMessage.fromJson(response.first));
-      }});
+    void callback(XIMU3_CharArrays data, ffi.Pointer<ffi.Void> context) {
+      completer.complete(data);
     }
+
+    final List<String> strings = List.generate(commands.length, (index) => commands[index].toJson());
+    ffi.Pointer<ffi.Pointer<Utf8>> pointer = FFIHelpers.convertListToPointerPointer(strings);
+    _instance.api.XIMU3_connection_send_commands_async(connection, pointer.cast(), commands.length, 2, 500, NativeCallable<
+        ffi.Void Function(XIMU3_CharArrays data,
+            ffi.Pointer<ffi.Void> context)>.listener(callback)
+        .nativeFunction,
+        ffi.nullptr);
+    calloc.free(pointer);
+
+    await completer.future.then((XIMU3_CharArrays data) {
+      List<String> strings = _toListOfStringsAndFree(data);
+      responses = List.generate(strings.length, (index) => CommandMessage.fromJson(strings[index]));
+    });
 
     return responses;
   }
 
-  // TODO: Fix for char arrays containing multiple strings
   static List<String> _toListOfStringsAndFree(XIMU3_CharArrays charArrays) {
     List<String> strings = [];
     for (int i = 0; i < charArrays.length; i++) {
-      strings.add((charArrays.array + i).cast<Utf8>().toDartString());
+      strings.add((charArrays.array + (i * XIMU3_CHAR_ARRAY_SIZE) ~/ 8).cast<Utf8>().toDartString()); // TODO: Why does this work?
     }
      _instance.api.XIMU3_char_arrays_free(charArrays);
     return strings;
